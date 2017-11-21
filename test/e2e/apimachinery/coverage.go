2862a3a1625902ded266f5ccbc82ef12fa0fbcf5
Haowei Cai <haoweic@google.com>
2017-11-21 14:55:24 -0800

    Clean up and add resource-operation crawler result.

--- test/e2e/apimachinery/coverage.go
+++ test/e2e/apimachinery/coverage.go
@@ -115,7 +115,7 @@ var _ = SIGDescribe("Coverage", func() {
 
 		gvr := schema.GroupVersionResource{Version: "v1", Resource: "pods"}
 		By("reading yaml file data")
-		file, err := os.Open(filepath.Join(framework.TestContext.RepoRoot, "test/e2e/apimachinery", "pod.yaml"))
+		file, err := os.Open(filepath.Join(framework.TestContext.RepoRoot, "test/e2e/apimachinery/testdata", "pod.yaml"))
 		Expect(err).ToNot(HaveOccurred())
 		reader := yaml.NewYAMLReader(bufio.NewReader(file))
 		yamlPod, err := reader.Read()
@@ -143,67 +143,4 @@ var _ = SIGDescribe("Coverage", func() {
 		Expect(err).ToNot(HaveOccurred())
 		glog.Infof(">>>>>>pod list: %v", podList)
 	})
-
-	// It("should return chunks of results for list calls", func() {
-	// 	ns := f.Namespace.Name
-	// 	c := f.ClientSet
-	// 	client := c.CoreV1().PodTemplates(ns)
-
-	// 	By("creating a large number of resources")
-	// 	workqueue.Parallelize(20, numberOfTotalResources, func(i int) {
-	// 		for tries := 3; tries >= 0; tries-- {
-	// 			_, err := client.Create(&v1.PodTemplate{
-	// 				ObjectMeta: metav1.ObjectMeta{
-	// 					Name: fmt.Sprintf("template-%04d", i),
-	// 				},
-	// 				Template: v1.PodTemplateSpec{
-	// 					Spec: v1.PodSpec{
-	// 						Containers: []v1.Container{
-	// 							{Name: "test", Image: "test2"},
-	// 						},
-	// 					},
-	// 				},
-	// 			})
-	// 			if err == nil {
-	// 				return
-	// 			}
-	// 			framework.Logf("Got an error creating template %d: %v", i, err)
-	// 		}
-	// 		Fail("Unable to create template %d, exiting", i)
-	// 	})
-
-	// 	By("retrieving those results in paged fashion several times")
-	// 	for i := 0; i < 3; i++ {
-	// 		opts := metav1.ListOptions{}
-	// 		found := 0
-	// 		var lastRV string
-	// 		for {
-	// 			opts.Limit = int64(rand.Int31n(numberOfTotalResources/10) + 1)
-	// 			list, err := client.List(opts)
-	// 			Expect(err).ToNot(HaveOccurred())
-	// 			framework.Logf("Retrieved %d/%d results with rv %s and continue %s", len(list.Items), opts.Limit, list.ResourceVersion, list.Continue)
-
-	// 			if len(lastRV) == 0 {
-	// 				lastRV = list.ResourceVersion
-	// 			}
-	// 			if lastRV != list.ResourceVersion {
-	// 				Expect(list.ResourceVersion).To(Equal(lastRV))
-	// 			}
-	// 			for _, item := range list.Items {
-	// 				Expect(item.Name).To(Equal(fmt.Sprintf("template-%04d", found)))
-	// 				found++
-	// 			}
-	// 			if len(list.Continue) == 0 {
-	// 				break
-	// 			}
-	// 			opts.Continue = list.Continue
-	// 		}
-	// 		Expect(found).To(BeNumerically("==", numberOfTotalResources))
-	// 	}
-
-	// 	By("retrieving those results all at once")
-	// 	list, err := client.List(metav1.ListOptions{Limit: numberOfTotalResources + 1})
-	// 	Expect(err).ToNot(HaveOccurred())
-	// 	Expect(list.Items).To(HaveLen(numberOfTotalResources))
-	// })
 })
